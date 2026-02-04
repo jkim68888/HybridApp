c1b8b2b708b6c38fef03e609309ab3070ef0be71
Jihyun <jkim68888@gmail.com>
2026-02-04 20:21:41 +0900

    feat: 웹뷰 내비게이션 기능 구현
    - 뒤로가기,앞으로가기,새로고침,홈으로이동 기능 개발

--- screens/BrowserScreen.tsx
+++ screens/BrowserScreen.tsx
@@ -1,22 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useMemo, useRef } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useMemo, useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WebView from 'react-native-webview'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParams } from '../routes'
-import { useState } from 'react'
 import { Animated } from 'react-native'
+import { FontAwesome6 } from '@react-native-vector-icons/fontawesome6'
+import type { FontAwesome6SolidIconName } from '@react-native-vector-icons/fontawesome6'
 
 type Props = NativeStackScreenProps<RootStackParams, 'BrowserScreen'>;
 
-const BrowserScreen = ({ route }: Props) => {
+const NavButton = ({
+  iconName,
+  disabled,
+  onPress,
+}: {
+  iconName: FontAwesome6SolidIconName
+  disabled?: boolean
+  onPress: () => void
+}) => {
+  return (
+    <TouchableOpacity style={styles.button} disabled={disabled} onPress={onPress}>
+      <FontAwesome6 name={iconName} size={20} color={disabled ? 'lightgray' : 'black'} iconStyle="solid" />
+    </TouchableOpacity>
+  )
+}
+
+const BrowserScreen = ({ route, navigation }: Props) => {
   const { url } = route.params
   const [currentUrl, setCurrentUrl] = useState(url)
   const urlTitle = useMemo(() => {
     return currentUrl.replace('https://', '').split('/')[0]
   }, [currentUrl])
-
   const progress = useRef(new Animated.Value(0)).current
+  const webViewRef = useRef<WebView>(null)
+  const [canGoBack, setCanGoBack] = useState(false)
+  const [canGoForward, setCanGoForward] = useState(false)
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -27,8 +46,13 @@ const BrowserScreen = ({ route }: Props) => {
         <Animated.View style={[styles.loadingBar, { width: progress.interpolate({ inputRange: [0, 1], outputRange: ['0%', '100%'] }) }]} />
       </View>
       <WebView 
+        ref={webViewRef}
         source={{ uri: url }} 
-        onNavigationStateChange={({ url }) => setCurrentUrl(url)} 
+        onNavigationStateChange={(event) => {
+          setCurrentUrl(event.url)
+          setCanGoBack(event.canGoBack)
+          setCanGoForward(event.canGoForward)
+        }}
         onLoadProgress={({ nativeEvent }) => {
           console.log(nativeEvent.progress)
           progress.setValue(nativeEvent.progress)
@@ -37,6 +61,16 @@ const BrowserScreen = ({ route }: Props) => {
           progress.setValue(0)
         }}
       />
+      <View style={styles.navigator}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+          <View style={styles.naverIconOutline}>
+            <Text style={styles.naverIconText}>N</Text>
+          </View>
+        </TouchableOpacity>
+        <NavButton iconName="arrow-left" disabled={!canGoBack} onPress={() => webViewRef.current?.goBack()} />
+        <NavButton iconName="arrow-right" disabled={!canGoForward} onPress={() => webViewRef.current?.goForward()} />
+        <NavButton iconName="arrow-rotate-right" onPress={() => webViewRef.current?.reload()} />
+      </View>
     </SafeAreaView>   
   )
 }
@@ -60,5 +94,29 @@ const styles = StyleSheet.create({
   loadingBar: {
     height: 2,
     backgroundColor: 'blue',
+  },
+  navigator: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    paddingHorizontal: 40,
+  },
+  button: {
+    width: 30,
+    height: 30,
+  },
+  naverIconOutline: {
+    width: 30,
+    height: 30,
+    borderRadius: 4,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  naverIconText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold', 
   },
 })
